62e00c34e779f17ca093b237f9869676f19895cb
marklyfie4 <173835932+marklyfied@users.noreply.github.com>
2026-08-22 22:23:11 -0400

    you start with 125k
    you start with 125k money now instead of 75k

--- Content.Shared/Preferences/HumanoidCharacterProfile.cs
+++ Content.Shared/Preferences/HumanoidCharacterProfile.cs
@@ -34,7 +34,7 @@ namespace Content.Shared.Preferences
         public const int MaxLoadoutNameLength = 32;
         public const int MaxDescLength = 512;
 
-        public const int DefaultBalance = 75000;
+        public const int DefaultBalance = 125000;
 
         //private readonly Dictionary<string, JobPriority> _jobPriorities; // Frontier: commented out during merge.
         //private readonly List<string> _antagPreferences; // Frontier: commented out during merge.
